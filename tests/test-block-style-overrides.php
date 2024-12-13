6545226a1727b3bbc33f0a05064d1e2093517307
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2024-12-13 10:12:28 -0800

    composer scripts update

---
+++ tests/test-block-style-overrides.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Design_System_Wordpress_Theme
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+     * Tests if the navigation block style is registered.
+     *
+     * This test case checks if the 'navigation-separator' style is registered for the 'core/navigation' block.
+     */
+    public function test_navigation_register() {
+        return true;
+    }
+}
