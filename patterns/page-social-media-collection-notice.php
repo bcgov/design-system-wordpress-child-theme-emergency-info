cc017b4ce126a2ec40a926f02224003fb8605a48
Michael Haswell <Michael.Haswell@gov.bc.ca>
2025-03-18 13:59:35 -0700

    Add static page patterns Add static page templates using above patterns Add hazard images to assets Create top-banner template part used on static pages

---
+++ patterns/page-social-media-collection-notice.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Title: Social Media Collection Notice (Page)
+ * Slug: design-system-wordpress-child-theme-emergency-info/page-social-media-collection-notice
+ * Categories: page
+ *
+ * @package Design-System-WordPress-Theme
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--30);margin-bottom:var(--wp--preset--spacing--30);">
+    <!-- wp:group {"layout":{"type":"default"}} -->
+    <div class="wp-block-group"><!-- wp:group {"className":"alignwide","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20"}}},"layout":{"type":"constrained"}} -->
+        <div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)">
+            <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"default"}} -->
+            <div class="wp-block-group">
+
+                <!-- wp:paragraph -->
+                <p>The personal information you post may be collected by the Emergency Management and Climate Readiness under s. 26(c) of the Freedom of Information and Protection of Privacy Act for the purposes of engaging and consulting with the public. Please be aware that any information collected may be stored and/or accessed outside of Canada on servers not belonging to the government.</p>
+                <!-- /wp:paragraph -->
+
+                <!-- wp:paragraph -->
+                <p>To protect your own privacy and the privacy of others, please do not include any personal information including phone numbers and email addresses in the body of your comment. Please do not share personal information about others, including pictures.</p>
+                <!-- /wp:paragraph -->
+
+                <!-- wp:paragraph -->
+                <p>If you have any questions about the collection of your personal information, please contact:</p>
+                <!-- /wp:paragraph -->
+
+                <!-- wp:paragraph -->
+                <p>Devon McDonald, Response Information<br>Ministry of Emergency Management &amp; Climate Readiness<br>Devon.McDonald@gov.bc.ca</p>
+                <!-- /wp:paragraph -->
+
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
---
+++ patterns/page-territorial-acknowledgement.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Title: Territorial Acknowledgement (Page)
+ * Slug: design-system-wordpress-child-theme-emergency-info/page-territorial-acknowledgement
+ * Categories: page
+ *
+ * @package Design-System-WordPress-Theme
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--30);margin-bottom:var(--wp--preset--spacing--30);">
+    <!-- wp:group {"layout":{"type":"default"}} -->
+    <div class="wp-block-group"><!-- wp:group {"className":"alignwide","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20"}}},"layout":{"type":"constrained"}} -->
+        <div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)">
+            <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"default"}} -->
+            <div class="wp-block-group">
+
+                <!-- wp:paragraph -->
+                <p>
+                    The B.C. Public Service acknowledges the territories of First Nations around B.C. and is grateful to carry out our work on these lands. We acknowledge the rights, interests, priorities, and concerns of all Indigenous Peoples – First Nations, Métis, and Inuit – respecting and acknowledging their distinct cultures, histories, rights, laws, and governments.
+                </p>
+                <!-- /wp:paragraph -->
+
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
