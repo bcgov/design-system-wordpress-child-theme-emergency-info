16bdcc2f74ad43926c0355683d1559c5ee806611
Michael Haswell <Michael.Haswell@gov.bc.ca>
2025-12-10 15:13:05 -0800

    Add pattern header Fix images missing file extension Bump theme version in style.css (forgot to do this last release)

--- patterns/tornado.php
+++ patterns/tornado.php
@@ -1,70 +1,14 @@
+<?php
+/**
+ * Title: Tornado
+ * Slug: design-system-wordpress-child-theme-emergency-info/tornado
+ * Categories: event, tornado
+ *
+ * @package Design-System-WordPress-Theme
+ */
+?>
+
 <!-- wp:pattern {"slug":"design-system-wordpress-child-theme-emergency-info/event-header"} /-->
-
-<!-- wp:group {"templateLock":false,"lock":{"move":false,"remove":false},"className":"hazard-border","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}},"border":{"top":{"width":"10px"},"radius":"10px"}},"backgroundColor":"support-gray-01","layout":{"type":"default"}} -->
-<div id="Event-Information" class="wp-block-group hazard-border has-support-gray-01-background-color has-background"
-    style="border-radius:10px;border-top-width:10px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
-    <!-- wp:group {"style":{"border":{"radius":{"bottomLeft":"10px","bottomRight":"10px"},"right":{"color":"var:preset|color|support-gray-02"},"bottom":{"color":"var:preset|color|support-gray-02"},"left":{"color":"var:preset|color|support-gray-02"}},"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20"}}},"layout":{"type":"default"}} -->
-    <div class="wp-block-group"
-        style="border-bottom-left-radius:10px;border-bottom-right-radius:10px;border-right-color:var(--wp--preset--color--support-gray-02);border-bottom-color:var(--wp--preset--color--support-gray-02);border-left-color:var(--wp--preset--color--support-gray-02);padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)">
-        <!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-        <div class="wp-block-group" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
-            <!-- wp:group {"className":"d-none d-sm-block","style":{"spacing":{"padding":{"top":"0","right":"var:preset|spacing|20","bottom":"0","left":"0"}}},"layout":{"type":"default"}} -->
-            <div class="wp-block-group d-none d-sm-block"
-                style="padding-top:0;padding-right:var(--wp--preset--spacing--20);padding-bottom:0;padding-left:0">
-                <!-- wp:emergency-info/post-hazard-image /--></div>
-            <!-- /wp:group -->
-
-            <!-- wp:group {"layout":{"type":"default"}} -->
-            <div class="wp-block-group">
-                <!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}},"typography":{"fontStyle":"normal","fontWeight":"700"}},"layout":{"type":"default"}} -->
-                <div class="wp-block-group"
-                    style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;font-style:normal;font-weight:700">
-                    <!-- wp:emergency-info/post-hazard-title {"className":"m-0","fontSize":"extra-large"} /--></div>
-                <!-- /wp:group -->
-
-                <!-- wp:post-title {"level":1,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"extra-large"} /-->
-
-                <!-- wp:emergency-info/post-emergency-alert {"fontSize":"medium"} /-->
-            </div>
-            <!-- /wp:group -->
-
-            <!-- wp:group {"className":"state-of-emergency-bcgov-logo","layout":{"type":"constrained"}} -->
-            <div class="wp-block-group state-of-emergency-bcgov-logo"><!-- wp:paragraph -->
-                <p></p>
-                <!-- /wp:paragraph -->
-            </div>
-            <!-- /wp:group -->
-        </div>
-        <!-- /wp:group -->
-
-        <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","right":"0","bottom":"0","left":"0"}}},"layout":{"type":"default"}} -->
-        <div class="wp-block-group"
-            style="padding-top:var(--wp--preset--spacing--20);padding-right:0;padding-bottom:0;padding-left:0">
-            <!-- wp:group {"className":"justify-content-between","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","right":"0","bottom":"0","left":"0"}},"border":{"top":{"color":"var:preset|color|support-gray-02"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-            <div class="wp-block-group justify-content-between"
-                style="border-top-color:var(--wp--preset--color--support-gray-02);padding-top:var(--wp--preset--spacing--20);padding-right:0;padding-bottom:0;padding-left:0">
-                <!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"layout":{"type":"flex","flexWrap":"wrap"}} -->
-                <div class="wp-block-group" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
-                    <!-- wp:paragraph {"fontSize":"small"} -->
-                    <p class="has-small-font-size">Updated on</p>
-                    <!-- /wp:paragraph -->
-
-                    <!-- wp:mfb/meta-field-block {"fieldType":"acf","fieldName":"updated_date","displayLayout":"inline-block","style":{"spacing":{"padding":{"right":"0.2rem","left":"0.2rem"}}},"fontSize":"small"} /-->
-
-                    <!-- wp:mfb/meta-field-block {"fieldType":"acf","fieldName":"updated_time","displayLayout":"inline-block","fontSize":"small"} /-->
-                </div>
-                <!-- /wp:group -->
-
-                <!-- wp:emergency-info/post-social-share /-->
-            </div>
-            <!-- /wp:group -->
-        </div>
-        <!-- /wp:group -->
-    </div>
-    <!-- /wp:group -->
-</div>
-<!-- /wp:group --></div>
-<!-- /wp:group -->
 
 <!-- wp:group {"templateLock":false,"lock":{"move":false,"remove":false},"className":"hazard-border","style":{"border":{"left":{"width":"10px"}},"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|30"},"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"layout":{"type":"default"}} -->
 <div id="Event-Summary" class="wp-block-group hazard-border"
@@ -79,7 +23,7 @@
         <!-- wp:paragraph -->
         <p>Environment and Climate Change Canada has issued a <strong>Tornado [Warning / Watch]</strong> for [Location].
             This is a dangerous and potentially life-threatening situation. Take cover immediately if threatening
-            weather approache<em>s.</em></p>
+            weather approaches.</p>
         <!-- /wp:paragraph -->
     </div>
     <!-- /wp:group -->
@@ -396,7 +340,7 @@
                             style="padding-top:var(--wp--preset--spacing--20)">
                             <!-- wp:image {"id":3365,"sizeSlug":"full","linkDestination":"none"} -->
                             <figure class="wp-block-image size-full"><img
-                                    src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/route-sign' ); ?>"
+                                    src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/route-sign.png' ); ?>"
                                     alt="" class="wp-image-3365" title="" /></figure>
                             <!-- /wp:image -->
                         </div>
@@ -516,7 +460,7 @@
                             style="padding-top:var(--wp--preset--spacing--20)">
                             <!-- wp:image {"id":3365,"sizeSlug":"full","linkDestination":"none"} -->
                             <figure class="wp-block-image size-full"><img
-                                    src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/route-sign' ); ?>"
+                                    src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/route-sign.png' ); ?>"
                                     alt="" class="wp-image-3365" title="" /></figure>
                             <!-- /wp:image -->
                         </div>
@@ -629,7 +573,7 @@
         style="border-radius:10px;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)">
         <span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img
             class="wp-block-cover__image-background wp-image-53712" alt=""
-            src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/extreme-cold-map' ); ?>"
+            src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/extreme-cold-map.png' ); ?>"
             data-object-fit="cover" />
         <div class="wp-block-cover__inner-container">
             <!-- wp:group {"style":{"border":{"radius":"10px"},"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"},"margin":{"bottom":"var:preset|spacing|20"}},"elements":{"link":{"color":{"text":"var:preset|color|text-color"}}}},"textColor":"text-color","layout":{"type":"constrained","justifyContent":"left","wideSize":"400px"}} -->
@@ -683,7 +627,7 @@
                 <figure class="wp-block-gallery has-nested-images columns-default is-cropped">
                     <!-- wp:image {"id":35797,"sizeSlug":"full","linkDestination":"none"} -->
                     <figure class="wp-block-image size-full"><img
-                            src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/extreme-cold' ); ?>"
+                            src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/extreme-cold.png' ); ?>"
                             alt="" class="wp-image-35797" title="" /></figure>
                     <!-- /wp:image -->
                 </figure>
@@ -819,7 +763,7 @@
                 style="padding-top:var(--wp--preset--spacing--20)">
                 <!-- wp:image {"id":2371,"sizeSlug":"full","linkDestination":"none"} -->
                 <figure class="wp-block-image size-full"><img
-                        src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/checklist' ); ?>"
+                        src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/checklist.png' ); ?>"
                         alt="" class="wp-image-2371" title="" /></figure>
                 <!-- /wp:image -->
             </div>
